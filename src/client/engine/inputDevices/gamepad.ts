60dd5430406c67d4ceb48716919d6ea790ca746e
nathan.hitchcock <tripnull@gmail.com>
2017-05-20 16:32:44 -0700

    [wip] gamepad event management

--- src/client/engine/inputDevices/gamepad.ts
+++ src/client/engine/inputDevices/gamepad.ts
@@ -3,65 +3,114 @@
  *  and sending the connected/disconnected events.
  */
 
-export const gamepads: Gamepad[] = [];
+const GamepadButtonLookup = {
+    0: 'a',
+    1: 'b',
+    2: 'x',
+    3: 'y',
+    4: 'l1',
+    5: 'r1',
+    6: 'l2',
+    7: 'r2',
+    10: 'l3',
+    11: 'r3',
+    12: 'dup',
+    13: 'ddown',
+    14: 'dleft',
+    15: 'dright',
+};
+
+const rawGamepads: Gamepad[] = [];
+const rawGamepadEventers: GamepadEventer[] = [];
 
 window.addEventListener('gamepadconnected', e => {
     const index = (e as any).gamepad.index;
     const gamepad = navigator.getGamepads()[index];
-    gamepads[index] = gamepad;
+    rawGamepads[index] = gamepad;
+    rawGamepadEventers[index] = new GamepadEventer(gamepad);
 
     console.log("connection event for " + index);
 });
 
 window.addEventListener('gamepaddisconnected', e => {
     const index = (e as any).gamepad.index;
-    delete gamepads[index];
+    delete rawGamepads[index];
+    delete rawGamepadEventers[index];
 
     console.log("disconnection event for " + index);
 });
 
 class GamepadEventSets {
-    onPressed: Array<(gamepadId: number, value: number) => void>;
+    onPressed: Array<(value: number) => void>;
 
     constructor() {
         this.onPressed = [];
     }
+
+    copy(source: GamepadEventSets) {
+        this.onPressed = [];
+        for(const event of source.onPressed) this.onPressed.push(event);
+        return this;
+    }
 }
 
-declare class GamepadEventMap { [key: string]: GamepadEventSets }
+class GamepadEventMap {
+    [key: string]: GamepadEventSets
+}
 
-// [TODO] decide if I should mash axis and buttons into the same map
-const gamepadButtonEvents: GamepadEventMap = {};
-const gamepadAxesEvents: GamepadEventMap = {};
-const gamepadConnectedEvents: Array<(gamepadId: number)=>void> = [];
-const gamepadDisonnectedEvents: Array<(gamepadId: number)=>void> = [];
+const gamepadConnectedEvents: Array<(gamepad: GamepadEventer)=>void> = [];
+const gamepadDisonnectedEvents: Array<(gamepad: GamepadEventer)=>void> = [];
 
+const gamepadEventerTemplate: GamepadEventMap = {};
 
-function FireGamepadEvents(eventMap: GamepadEventMap,
-                           gamepadId: number,
-                           button: string | number,
-                           value: number) {
+export class GamepadEventer {
+    gamepad: Gamepad;
+    gamepadEvents: GamepadEventMap;
+
+    constructor(gamepad: Gamepad) {
+        this.gamepadEvents = new GamepadEventMap();
+        for(const event in gamepadEventerTemplate) {
+            if(gamepadEventerTemplate[event]) {
+                this.gamepadEvents[event] = new GamepadEventSets().copy(gamepadEventerTemplate[event]);
+            }
+        }
+    }
+
+    // Handle buttons, axes, analog triggers, etc
+    // maybe can take which gamepadId to listen to, and generate a closure to filter by that id?
+    // is there a more efficient way to do that than filtering every event? Like storing in a sparse array?
+    on(eventType: string | number, handler: (value: number) => void) {
+        const lookUp = GamepadButtonLookup[eventType] || (eventType as string).toLowerCase();
+        this.gamepadEvents[lookUp].onPressed.push(handler);
+    }
+}
+
+function FireGamepadEvents(gamepad: GamepadEventer, button: string | number, value: number) {
+    const eventMap = gamepad.gamepadEvents;
     if(eventMap[button]) {
         let eventSet;
         eventSet = eventMap[button].onPressed;
-        eventSet.forEach(func => func(gamepadId, value));
+        eventSet.forEach(func => func(value));
     }
 }
+
+const gamepadEventers: GamepadEventer[] = [];
 
 function tick() {
     requestAnimationFrame(tick);
 
     // Handle gamepads
     const gpds = navigator.getGamepads();
-    for(let i = 0; i < gpds.length; ++i) {
-        if(gpds[i]) {
-            for(let a = 0; a < gpds[i].axes.length; ++a) {
-                FireGamepadEvents(gamepadAxesEvents, i, a, gpds[i].axes[a]);
+
+    for(const gamepadEventer of gamepadEventers) {
+        if(gamepadEventer) {
+            const gp = gamepadEventer.gamepad;
+            for(let a = 0; a < gp.axes.length; ++a) {
+                FireGamepadEvents(gamepadEventer, 'axis'+a, gp.axes[a]);
             }
-            for(let b = 0; b < gpds[i].buttons.length; ++b) {
-                const button = gpds[i].buttons[b];
-                FireGamepadEvents(gamepadButtonEvents, i, b,
-                                button.value !== undefined ? button.value : (button.pressed?1:0));
+            for(let b = 0; b < gp.buttons.length; ++b) {
+                const button = gp.buttons[b];
+                FireGamepadEvents(gamepadEventer, b, button.value !== undefined ? button.value : (button.pressed?1:0));
             }
         }
     }
@@ -69,7 +118,7 @@ function tick() {
 
 tick();
 
-export function onGamepadConnected(handler: (gamepadId: number) => void) {
+export function onGamepadConnected(handler: (gamepad: GamepadEventer) => void) {
     return gamepadConnectedEvents.push(handler);
 }
 
@@ -77,34 +126,10 @@ export function offGamepadConnected(handlerId: number) {
     delete gamepadConnectedEvents[handlerId];
 }
 
-export function onGamepadDisconnected(handler: (gamepadId: number) => void) {
+export function onGamepadDisconnected(handler: (gamepad: GamepadEventer) => void) {
     return gamepadDisonnectedEvents.push(handler);
 }
 
 export function offGamepadDisconnected(handlerId: number) {
     delete gamepadDisonnectedEvents[handlerId];
-}
-
-// Handle buttons, axes, analog triggers, etc
-// maybe can take which gamepadId to listen to, and generate a closure to filter by that id?
-// is there a more efficient way to do that than filtering every event? Like storing in a sparse array?
-export function onGamepadInputEvent(gamepadId: number, eventType: string | number, handler: (value: number) => void) {
-    // parse event type (axes/button)
-    if(typeof eventType === "string") {
-        // Axis mapping, or string button name to index mapping
-        if(eventType.substr(0, 4) === "axis") {
-            const axisNum = eventType.slice(4);
-            gamepadAxesEvents.onPressed[axisNum].push((gpId: number, value: number)=> {
-                if(gpId === gamepadId) handler(value);
-            });
-        } else {
-            // [TODO] Do button to index mapping
-        }
-    } else {
-        // Raw button index mapping
-        const buttonId: number = eventType;
-        gamepadButtonEvents.onPressed[buttonId].push((gpId: number, value: number)=> {
-            if(gpId === gamepadId) handler(value);
-        });
-    }
 }
